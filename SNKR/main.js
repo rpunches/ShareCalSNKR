9d2b46a63d4e25478456617af8c49e0dffa5a784
Rachel McDermott <rpunches@gmail.com>
2018-03-11 16:03:22 -0500

    consolidated pages and code

--- SNKR/main.js
+++ SNKR/main.js
@@ -1,3 +1,79 @@
+// CALENDAR JQUERY
+
+$(document).ready(function () {
+
+    $('#calendar').fullCalendar({
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay,listWeek'
+        },
+        defaultDate: '2018-03-12',
+        navLinks: true, // can click day/week names to navigate views
+        editable: true,
+        eventLimit: true, // allow "more" link when too many events
+        events: [
+            {
+                title: 'All Day Event',
+                start: '2018-03-01',
+            },
+            {
+                title: 'Long Event',
+                start: '2018-03-07',
+                end: '2018-03-10'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2018-03-09T16:00:00'
+            },
+            {
+                id: 999,
+                title: 'Repeating Event',
+                start: '2018-03-16T16:00:00'
+            },
+            {
+                title: 'Conference',
+                start: '2018-03-11',
+                end: '2018-03-13'
+            },
+            {
+                title: 'Meeting',
+                start: '2018-03-12T10:30:00',
+                end: '2018-03-12T12:30:00'
+            },
+            {
+                title: 'Lunch',
+                start: '2018-03-12T12:00:00'
+            },
+            {
+                title: 'Meeting',
+                start: '2018-03-12T14:30:00'
+            },
+            {
+                title: 'Happy Hour',
+                start: '2018-03-12T17:30:00'
+            },
+            {
+                title: 'Dinner',
+                start: '2018-03-12T20:00:00'
+            },
+            {
+                title: 'Birthday Party',
+                start: '2018-03-13T07:00:00'
+            },
+            {
+                title: 'Click for Google',
+                url: 'http://google.com/',
+                start: '2018-03-28'
+            }
+        ]
+    });
+
+});
+
+
+//   BUTTON JQUERY
 $(".button-container").on("click", function () {
 
     var sign = $(this).attr("data-sign");
@@ -21,7 +97,8 @@ $(".button-container").on("click", function () {
             var p = $("<p>").text("This is " + sign + "'s (" + data.date_range + ") " + "horoscope for " + data.current_date + ": " + scope);
 
 
-            hsDiv.append(p, color, time, number, mood);
+            hsDiv.append(p);
+            // hsDiv.append(p, color, time, number, mood);
             // color.append(number, time);
             // time.append(mood);
             $("#reading-appear-here").prepend(hsDiv);
